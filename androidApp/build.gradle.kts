a77f164dd978fb372f2ee83836ea96548b7f03e0
Aditya More <aditya1875more@gmail.com>
2026-05-21 02:58:20 +0530

    Update application versionCode and versionName

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         targetSdk = 36
 
         applicationId = "org.aditya1875.facenox.androidApp"
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
     }
 
     compileOptions {
